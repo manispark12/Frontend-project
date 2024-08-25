6e01de8be0badbe47b12e8b7eec4dfcd4d4baefe
mani <a.manikanta9810@gmail.com>
2024-08-25 21:52:04 +0530

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,21 @@
-export const Cart = [];
+ export const Cart = [];
+
+ export function addToCart(productId){
+    let matchingItem;
+      Cart.forEach((cartItem) =>{
+        if(productId === cartItem.productId){
+          matchingItem = cartItem;
+        }
+        
+      });
+      if(matchingItem){
+        matchingItem.quantity += 1;
+      }
+      else{
+        Cart.push({
+          productId: productId,
+          quantity: 1
+        });
+  
+      }
+  }
