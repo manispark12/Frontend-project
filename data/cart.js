463bb7e93b255a5d5693cc9bda3e39b521fae56e
mani <a.manikanta9810@gmail.com>
2024-08-26 22:14:59 +0530

    Save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,24 @@
- export let Cart = [{
+ export let Cart = JSON.parse(localStorage.getItem('Cart'));
+
+ if(!Cart){
+  Cart = 
+  [{
     productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
+    deliveryOptionId:'1'
  },
  {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity:1
+    quantity:1,
+    deliveryOptionId:'2'
  }
 
  ];
+ }
+ 
+ function saveToStorage(){
+  localStorage.setItem('Cart',JSON.stringify(Cart));
+ }
 
  export function addToCart(productId){
     let matchingItem;
@@ -23,10 +34,12 @@
       else{
         Cart.push({
           productId: productId,
-          quantity: 1
+          quantity: 1,
+          deliveryOptionId:'1'
         });
   
       }
+      saveToStorage();
   }
 
   export function removeFromCart(productId){
@@ -39,5 +52,6 @@
     });
 
     Cart = newCart;
+    saveToStorage();
 
   }
