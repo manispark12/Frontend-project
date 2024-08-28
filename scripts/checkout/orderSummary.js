5292a5a07d9c917870cb4d6dff0913950a778d17
mani <a.manikanta9810@gmail.com>
2024-08-28 22:14:03 +0530

    create automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -29,7 +29,8 @@ Cart.forEach((cartItem) =>{
    cartSummaryHTML +=
 
     `
-     <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+     <div class="cart-item-container js-cart-item-container
+      js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: ${dateString}
             </div>
@@ -45,14 +46,14 @@ Cart.forEach((cartItem) =>{
                 <div class="product-price">
                   $${formatCurrency(matchingProduct.priceCents)}
                 </div>
-                <div class="product-quantity">
+                <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                   <span>
                     Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                   </span>
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+                  <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -133,7 +134,7 @@ document.querySelectorAll('.js-delivery-option')
     renderPaymentSummary();
 
 
-  })
+  });
 
 });
 }
